dfcba784ae337304eb8bf4eb444e6bd2c78e5f0f
yagmurcetintas <yagmurcetin@gmail.com>
2019-06-25 20:17:52 -0400

    added initial files

---
+++ index.js
@@ -0,0 +1,8 @@
+//require inquirer
+const inquirer = require('inquirer');
+//require axios
+const axios = require('axios');
+//require chalk, give error messages red color
+const chalk = require('chalk');
+
+const word = require("./word.js");
